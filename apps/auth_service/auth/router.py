e4a87a214cb1e067b50f141a647efd63221ac133
ivan <ivan.zakharov@nordclan.com>
2026-02-18 17:30:16 +0400

    Чистая архитектура

--- apps/auth_service/auth/router.py
+++ apps/auth_service/auth/router.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from apps.core.database import AsyncSession, get_session
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/sign_up")
-async def sign_up():
+async def sign_up(session: Annotated[AsyncSession, Depends(get_session)],):
     ...
