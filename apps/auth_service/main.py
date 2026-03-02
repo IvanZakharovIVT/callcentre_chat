254bb8417c2811ee58208e89d9b634550f36e592
ivan <ivan.zakharov@nordclan.com>
2026-03-02 11:44:16 +0400

    Настройка прокси

--- apps/auth_service/main.py
+++ apps/auth_service/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 from apps.auth_service.auth.router import router as auth_router
 
 app = FastAPI(
-    root_path="/api/user",
-    # openapi_url="/openapi.json",
+    docs_url="/api/v1/docs",
+    redoc_url="/api/v1/redoc",
+    openapi_url="/api/v1/openapi.json",
     title="auth user swagger",
     version="1.0.0",
-    # openapi_version="3.1.0"
 )
 
 app.include_router(auth_router)
--- apps/chat_service/main.py
+++ apps/chat_service/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 from apps.chat_service.chat.router import router as chat_router
 
 app = FastAPI(
-    root_path="/api/chat",
-    # openapi_url="/openapi.json",
+    docs_url="/api/v1/docs",
+    redoc_url="/api/v1/redoc",
+    openapi_url="/api/v1/openapi.json",
     title="web_chat",
     version="1.0.0",
-    # openapi_version="3.1.0"
 )
 
 app.include_router(chat_router)
