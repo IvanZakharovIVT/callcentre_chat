61015c2d13853107df2c399adc512b7a3d0fec3e
ivan <ivan.zakharov@nordclan.com>
2026-03-13 15:29:14 +0400

    авторизация в админке

--- apps/auth_service/main.py
+++ apps/auth_service/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from starlette_admin.contrib.sqla import Admin
+from starlette.middleware.sessions import SessionMiddleware
 
 from apps.auth_service.admin.admin import UserAdmin
+from apps.auth_service.admin.auth import UsernameAndPasswordProvider
 from apps.auth_service.auth.models import User
 from apps.auth_service.auth.router import router as auth_router
 from apps.core.database import engine
@@ -14,10 +16,19 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="your-secret-key", # Replace with a strong, random key
+    # optional parameters
+    # https_only=True,
+    # max_age=3600 # seconds until the session expires
+)
+
 app.include_router(auth_router)
 
 # Create admin
-admin = Admin(engine, title="Example: SQLAlchemy")
+admin = Admin(engine, auth_provider=UsernameAndPasswordProvider(), title="Example: SQLAlchemy")
 
 # Add view
 admin.add_view(UserAdmin(User))
