9e332dd1b40677e19091eafbb1b4dff3c83b9362
ivan <ivan.zakharov@nordclan.com>
2026-03-13 17:33:33 +0400

    elastic

--- apps/core/services/elasticsearch_service.py
+++ apps/core/services/elasticsearch_service.py
@@ -1,8 +1,20 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import AsyncElasticsearch
 
 from apps.core.config import settings
 
 
 class ElasticsearchService:
     def __init__(self):
-        self._es = Elasticsearch(hosts=settings.ES_SERVER, basic_auth=(settings.ES_USER, settings.ES_PASSWORD))
+        self._es = AsyncElasticsearch(
+            hosts=settings.ES_SERVER,
+            # basic_auth=(settings.ES_USER, settings.ES_PASSWORD)
+        )
+
+    async def search(self, query):
+        return await self._es.search(
+            index='my_index',
+            d={"match_all": query}
+        )
+
+    async def add_document(self, document):
+        await self._es.index(index="my_index", body=document)
